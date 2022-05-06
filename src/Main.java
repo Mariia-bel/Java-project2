77c67ed4868cda9df7d97aa71157f24f47c09be1
Мария <airbelka@bk.ru>
2022-05-06 16:53:02 +0300

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-    int amount = 1600;    // стоимость билета
-    int rub = 20;    // рублей для одной бонусной милли
-    int mili = amount/rub;  // Рассчёт количества бонусных милль
+        int amount = 1600;    // стоимость билета
+        int rub = 20;    // рублей для одной бонусной милли
+        int mili = amount / rub;  // Рассчёт количества бонусных милль
         System.out.println("Спасибо за покупку. Вам начисленно");
         System.out.println((mili) + " миль");
     }
